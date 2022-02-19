364e70db53b29ece20912c608b56cc72e5522b1e
mikiya00 <77037921+mikiya00@users.noreply.github.com>
2022-02-19 17:48:46 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,20 +2,24 @@
 //document.querySelector("form")=instagram
 //document.querySelector("div")=twitter
 
+//use strict = 厳格モード
+
 'use strict';
 const imagePath = 'pic/head.png';
 const imageUrl = chrome.runtime.getURL(imagePath);
-const imgWithUrl = `<img src="${imageUrl}" style="width: 1600px; height: 150px; background-color: lightgray;"!important>`;
-document.querySelector("form").insertAdjacentHTML("afterEnd", imgWithUrl);
+const imgWithUrl = `<img src="${imageUrl}" style= "position: fixed; z-index: 1; width: 1600px; height: 150px; background-color: lightgray;"!important>`;
+document.querySelector("div").insertAdjacentHTML("beforebegin", imgWithUrl);
 
-/*
-サイド
+//サイド
 'use strict';
 const imagePathtwo = 'pic/said.png';
 const imageUrltwo = chrome.runtime.getURL(imagePathtwo);
-const imgWithUrltwo = `<img src="${imageUrltwo}" style="width: 200px; height: 30000px; background-color: lightgray;"!important>`;
-document.querySelector("img").insertAdjacentHTML("afterEnd", imgWithUrltwo);
-*/
+const imgWithUrltwo = `<img src="${imageUrltwo}" style= "position: fixed; margin-right:200px; margin-left:-300px; margin-top:50px; width: 250px; height: 700px; background-color: lightgray;"!important>`;
+var elements = document.getElementsByClassName('_1SP8R C3uDN j9XKR  ');
+    elements[0].insertAdjacentHTML("afterbegin", imgWithUrltwo);
+
+//メール風に宛先、件名等をつける
+document.querySelector("header ").insertAdjacentHTML('beforebegin', '<p>ランチを食べました</p>');
 
 // インスタ用
 //要素の削除
@@ -79,6 +83,16 @@ var elements = document.getElementsByClassName('b5itu');
 }
 
 
+//ナビを取得
+var elements = document.getElementsByClassName('NXc7H jLuN9');
+//ナビの削除まで
+while(elements.length != 0){
+  //ナビの要素を削除
+  elements[0].parentNode.removeChild(elements[0]);
+}
+
+
+
 
 //document.querySelector("css-1dbjc4n").innerHTML = "<h2>HAHAHAHAHAHAHAHAHAHAHAHAHAHAHA</span>";
 
@@ -86,7 +100,7 @@ var elements = document.getElementsByClassName('b5itu');
 
 //getElementById('id_Red')でvisual.htmlのidを取得onclickハンドラ
 //chrome.tabs(API)を使いタブの作成をし、query({})の中は最近開いたタブを取得
-
+/*
 document.getElementById('id_Red').onclick = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
@@ -109,3 +123,4 @@ function setBackGroundColorRed(){
 function setBackGroundColorBlue(){
   document.body.style.backgroundColor = 'blue';
 }
+*/
